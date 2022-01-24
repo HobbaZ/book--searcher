9986668f30d65432e1741b725a47e9a1f44dc8d3
HobbZ <zachobba@gmail.com>
2022-01-24 15:25:15 +1000

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dot-env').config();
+require('dot-env');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
